f1529cb62de89c034505768d1e844e9defb99aaa
Hiroyuki Adachi <hiroxpepe@gmail.com>
2020-07-01 22:01:10 +0900

    Fix the scripts to can move the player object. #5

--- Assets/Scripts/GamepadMaper.cs
+++ Assets/Scripts/GamepadMaper.cs
@@ -1,18 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Copyright 2002-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
-public class GamepadMaper : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+namespace Examproject {
+    /// <summary>
+    /// to map physical Gamepad.
+    /// @author h.adachi
+    /// </summary>
+    public class GamepadMaper : MonoBehaviour {
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // Fields
+
+        protected ButtonControl aButton;
+
+        protected ButtonControl bButton;
+
+        protected ButtonControl xButton;
+
+        protected ButtonControl yButton;
+
+        protected ButtonControl upButton;
+
+        protected ButtonControl downButton;
+
+        protected ButtonControl leftButton;
+
+        protected ButtonControl rightButton;
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // update Methods
+
+        // Start is called before the first frame update
+        protected void Start() {
+        }
+
+        // Update is called once per frame
+        protected void Update() {
+            mapGamepad();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // private methods
+
+        private void mapGamepad() {
+            // Identifies the OS.
+            upButton = Gamepad.current.dpad.up;
+            downButton = Gamepad.current.dpad.down;
+            leftButton = Gamepad.current.dpad.left;
+            rightButton = Gamepad.current.dpad.right;
+            if (Application.platform == RuntimePlatform.Android) {
+                // Android OS
+                aButton = Gamepad.current.aButton;
+                bButton = Gamepad.current.bButton;
+                xButton = Gamepad.current.xButton;
+                yButton = Gamepad.current.yButton;
+            } else if (Application.platform == RuntimePlatform.WindowsPlayer) {
+                // Windows OS
+                aButton = Gamepad.current.bButton;
+                bButton = Gamepad.current.aButton;
+                xButton = Gamepad.current.yButton;
+                yButton = Gamepad.current.xButton;
+            } else {
+                // FIXME: can't get it during development with Unity?
+                aButton = Gamepad.current.bButton;
+                bButton = Gamepad.current.aButton;
+                xButton = Gamepad.current.yButton;
+                yButton = Gamepad.current.xButton;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,111 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Copyright 2002-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+namespace Examproject {
+    /// <summary>
+    /// player controller.
+    /// @author h.adachi
+    /// </summary>
+    public class PlayerController : GamepadMaper {
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // Fields
+
+        private DoFixedUpdate doFixedUpdate;
+
+        private float speed;
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // update Methods
+
+        // Awake is called when the script instance is being loaded.
+        void Awake() {
+            doFixedUpdate = DoFixedUpdate.GetInstance();
+        }
+
+        // Start is called before the first frame update
+        new void Start() {
+        }
+
+        // Update is called once per frame
+        new void Update() {
+            base.Update();
+            if (upButton.isPressed) {
+                doFixedUpdate.walk = true;
+            }
+
+        }
+
+        // FixedUpdate is called just before each physics update.
+        void FixedUpdate() {
+            var _rb = transform.GetComponent<Rigidbody>(); // Rigidbody should be only used in FixedUpdate.
+            speed = _rb.velocity.magnitude; // get speed.
+
+            if (doFixedUpdate.walk) {
+                var _ADJUST1 = 0f;
+                _rb.AddFor​​ce(transform.forward * speed * _ADJUST1, ForceMode.Acceleration); // move forward.
+            }
+
+            doFixedUpdate.ResetMotion(); // initialize physical behavior flag.
+        }
+
+        #region DoFixedUpdate
+
+        /// <summary>
+        /// structure for the FixedUpdate() method.
+        /// </summary>
+        protected struct DoFixedUpdate {
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Fields
+
+            private bool _idol;
+            private bool _walk;
+
+            public bool idol { get => _idol; set => _idol = value; }
+            public bool walk { get => _walk; set => _walk = value; }
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Constructor
+
+            /// <summary>
+            /// returns an initialized instance.
+            /// </summary>
+            public static DoFixedUpdate GetInstance() {
+                var _instance = new DoFixedUpdate();
+                _instance.ResetMotion();
+                return _instance;
+            }
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // public Methods
+
+            /// <summary>
+            /// initialization of all fields.
+            /// </summary>
+            public void ResetMotion() {
+                _idol = false;
+                _walk = false;
+            }
+        }
+
+        #endregion
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
